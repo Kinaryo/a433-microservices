21be92d27e97ed60a478a03f009ec9017e2a32ae
Kinaryo <kinaryo733huda@gmail.com>
2023-11-25 14:37:40 +0700

    Adding the build_push_image_karsajobs.sh file to build and push the image

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -5,7 +5,7 @@
 docker build -t ghcr.io/Kinaryo/karsajobs:latest .
 
 # Log in to GitHub Container Registry
-docker login ghcr.io -u Kinaryo -p $GH_PACKAGES_TOKEN
+docker login ghcr.io -u Kinaryo -p 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
 
 # Push Docker image to GitHub Container Registry
 docker push ghcr.io/kinaryo/karsajobs:latest
